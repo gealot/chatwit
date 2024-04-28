b8e0a9be4b6b01fec5e43ece009d0b0246b8d76c
Peter Lee <gealot@gmail.com>
2024-04-29 00:32:01 +0900

    ♻️ Refactoring API Routes
    refactoring api routes for app router

--- app/api/list/route.ts
+++ app/api/list/route.ts
@@ -1,16 +1,20 @@
 import prisma from '@/libs/db';
-import { NextApiRequest, NextApiResponse } from 'next';
+import { NextRequest, NextResponse } from 'next/server';
 
 export const dynamic = 'force-dynamic';
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
+export async function GET(req: NextRequest) {
   try {
     const tweetList = await prisma.tweet.findMany();
+    console.log(tweetList);
     if (!tweetList) {
-      return res.status(404).json({ result: 'fail', message: '트윗이 하나도 없습니다.' });
+      return NextResponse.json({ status: 500, result: 'fail', message: '서버에 문제가 발생했습니다.' });
     }
-    return res.status(200).json({ result: 'success', tweetList });
+    if (tweetList.length === 0) {
+      return NextResponse.json({ status: 204, result: 'fail', message: '트윗이 하나도 없습니다.' });
+    }
+    return NextResponse.json({ status: 200, result: 'success', data: tweetList });
   } catch (error) {
     console.error('Error retrieving tweet list:', error);
-    return res.status(500).json({ result: 'fail', message: '서버에 문제가 발생했습니다.' });
+    return NextResponse.json({ status: 500, result: 'fail', message: '서버에 문제가 발생했습니다.' });
   }
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,45 @@
-const HomePage = async () => {
-/*
+'use client';
+
+import { useEffect, useState } from 'react';
+
+const HomePage = () => {
+  /*
 @TODO: 홈화면에 트윗 목록을 보여주기 위한 로직을 작성합니다.
 @EXAMPLE:
 const tweetList = await prisma.tweet.findMany();
 */
-// const tweetList = await getTweetList();
-// console.log(tweetList);
+  const [tweetList, setTweetList] = useState([]);
+  const handleList = async () => {
+    try {
+      const response = await fetch('/api/list');
+      if (response.ok) {
+        const data = await response.json();
+        if (data.data.length === 0) {
+          return setTweetList(data.message);
+        }
+        console.log(`[응답상태] ${response.status}`);
+        console.log(`[응답결과] ${data.result}`);
+        console.log(`[메시지] ${data.message}`);
+        console.log(`[데이터] ${data.data}`);
+        return setTweetList(data.data);
+      } else {
+        console.log(`[메시지] API 요청 실패`);
+      }
+    } catch (error) {
+      console.error(`[메시지] API 요청 오류:, ${error}`);
+    }
+  };
+  useEffect(() => {
+    handleList();
+  }, []);
   return (
     <main className="flex min-h-screen min-w-[60rem] max-w-full flex-col items-center justify-between">
-      {/* <div className="flex w-full max-w-5xl items-center justify-between text-sm">홈화면</div> */}
-      {/* <span>{JSON.stringify(tweetList)}</span> */}
+      <span>{tweetList}</span>
+      {/* {tweetList.map((tweet, index) => (
+        <div key={index}>
+          <p>{tweet}</p>
+        </div>
+      ))} */}
     </main>
   );
 };
